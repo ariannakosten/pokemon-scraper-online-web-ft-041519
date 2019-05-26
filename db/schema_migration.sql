bfd6edee144ffdb6b6c3d93b6dd068f36fa34b22
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-26 02:38:22 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -6,4 +6,7 @@ CREATE TABLE pokemon (
   name TEXT,
   type TEXT
 );
+<<<<<<< HEAD
 
+=======
+>>>>>>> 84fc67e7773d4f0826896feb28ad8736003b2a1b
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,15 +2,25 @@ require 'pry'
 
 class Pokemon
   
+<<<<<<< HEAD
   attr_accessor :name, :type, :db #:hp
   attr_reader :id
   
   def initialize(name:, type:, id: nil, db:)#, hp: nil)
+=======
+  attr_accessor :name, :type, :db
+  attr_reader :id
+  
+  def initialize(name:, type:, id: nil, db:)
+>>>>>>> 84fc67e7773d4f0826896feb28ad8736003b2a1b
     @name = name
     @type = type
     @id = id
     @db = db
+<<<<<<< HEAD
     #@hp = hp
+=======
+>>>>>>> 84fc67e7773d4f0826896feb28ad8736003b2a1b
   end
   
   def self.save(name, type, db)
@@ -22,6 +32,7 @@ class Pokemon
   end
 
   def self.find(id, db)
+<<<<<<< HEAD
     pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten  #<--returns a nested array b4
     name = pokemon[1] 
     type = pokemon[2]
@@ -32,3 +43,15 @@ class Pokemon
 #     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, id)
 #   end
   end
+=======
+     sql = <<-SQL
+      SELECT * FROM pokemon WHERE id = ?, id;
+    SQL
+     pokemon = db.execute(sql, id)
+     pokemon.name[1] 
+     pokemon.type[2]
+     
+     Pokemon.new(name, type, id, db)
+  end
+end
+>>>>>>> 84fc67e7773d4f0826896feb28ad8736003b2a1b
